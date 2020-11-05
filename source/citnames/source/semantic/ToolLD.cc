ca613c4b02c6502c2d00d36ddbed429ece33aa07
vikramsubramanian <vikram.subramanian6@gmail.com>
2020-11-04 19:21:19 -0500

    Aped ToolGcc.cc and ToolGcc.h to create ToolLD.cc and ToolLD.h. Next step should be figuring how to connect these 2 files with other scripts.

---
+++ source/citnames/source/semantic/ToolLD.cc
@@ -0,0 +1,42 @@
+/*  Copyright (C) 2012-2020 by László Nagy
+/*  Copyright (C) 2012-2020 by László Nagy
+    This file is part of Bear.
+
+    Bear is a tool to generate compilation database for clang tooling.
+
+    Bear is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Bear is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ToolWrapper.h"
+#include "ToolLD.h"
+
+#include <regex>
+
+namespace cs::semantic {
+
+    const char* ToolWrapper::name() const {
+        return "LD"; // UNSURE: Confirm this is just for debugging/logging
+    }
+
+    bool ToolWrapper::recognize(const fs::path& program) const {
+        static const auto pattern = std::regex(R"(^(LD)$)");
+
+        std::cmatch m;
+        return std::regex_match(program.filename().c_str(), m, pattern);
+    }
+
+    rust::Result<SemanticPtrs> ToolWrapper::compilations(const report::Command &command) const {
+        return ToolLD().compilations(command);
+    }
+}
